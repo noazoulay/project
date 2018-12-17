de30d94d950d475a27d09873adbafa2e65ffff82
noa <noazoulay1@gmail.com>
2018-12-17 22:28:34 +0200

    Initial commit

---
+++ Lexer.h
@@ -0,0 +1,24 @@
+//
+// Created by noa on 12/16/18.
+//
+
+#ifndef UNTITLED6_LEXER_H
+#define UNTITLED6_LEXER_H
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+
+class Lexer {
+
+public:
+
+    vector<string> lex(string name);
+
+};
+
+
+#endif //UNTITLED6_LEXER_H
